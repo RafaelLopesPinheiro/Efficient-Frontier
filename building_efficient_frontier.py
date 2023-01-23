2a62e93d8d84980a0030eb62cf10e4eb583485fd
RafaelLopesPinheiro <rafaellopespinheiro@hotmail.com>
2023-01-23 10:53:59 -0300

    Update building_efficient_frontier.py

--- building_efficient_frontier.py
+++ building_efficient_frontier.py
@@ -8,7 +8,7 @@ pd.set_option("display.max_rows", 100)
 
 def get_clean_data(stock_list, start, end, period='1D'): # Download and clean data from yahoo finance
     data = yf.Tickers(stock_list)
-    data = data.history(start=start, end=end)['Close']
+    data = data.history(start=start, end=end, period=period)['Close']
     data.sort_index(inplace=True)
     data.ffill(axis=0, inplace=True)
     return data
@@ -20,30 +20,26 @@ def efficient_frontier(cov_matrix, mean_daily_returns, num_portfolios, risk_free
         weights /= np.sum(weights)  # Divide by the sum of weights to make sum of array equals to 1.0
 
         portfolio_return = np.sum(mean_daily_returns *  weights) * 252
-        portfolio_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252) # Annualized std deviation (volatility)
+        portfolio_std_dev = np.sqrt(np.dot(weights.T, np.dot(cov_matrix, weights))) * np.sqrt(252)  # Annualized std deviation (volatility)
         
         results[0, i] = portfolio_return
         results[1, i] = portfolio_std_dev
-        results[2, i] = (results[0, i] - risk_free_rate)/ results[1, i] # Sharpe ratio formula
+        results[2, i] = (results[0, i] - risk_free_rate)/ results[1, i]  # Sharpe ratio formula
         
         for j in range(len(weights)):
             results[j+3, i] = weights[j]
  
 
 def plot_efficient_frontier(results_df, max_sharpe, min_vol):
-    #plotting the efficient frontier
     plt.figure(figsize=(15, 6))
     plt.scatter(results_df.Volatility, results_df.Return, c=results_df.Sharpe, cmap='YlGnBu')
     plt.xlabel('Annualized Volatility')
     plt.ylabel('Annualized Returns')
     plt.title("Efficient Frontier")
     plt.colorbar()
-
-
-    #plot a mark on the highest sharp ratio
+    
     plt.scatter(max_sharpe[1],max_sharpe[0], marker = (4,1,0), color='red', s=400, label='Max Sharpe ratio')
-    #plot a mark on the lowest std deviation portfolio
-    plt.scatter(min_volatility[1],min_volatility[0], marker = (4,1,0), color='black', s=400, label='Min volatility')
+    plt.scatter(min_vol[1],min_vol[0], marker = (4,1,0), color='black', s=400, label='Min volatility')
     plt.legend()
     plt.show()
     
